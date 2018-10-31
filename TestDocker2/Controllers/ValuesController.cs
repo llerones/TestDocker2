b7bb1e870162e0a2a8b82d6e92b3a6751fa42529
Luis Lerones <luis.lerones@just-eat.com>
2018-10-31 12:49:58 +0000

    Add project files.

---
+++ TestDocker2/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestDocker2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        public async Task<ActionResult> GetData()
+        {
+            return Ok();
+        }
+    }
+}
